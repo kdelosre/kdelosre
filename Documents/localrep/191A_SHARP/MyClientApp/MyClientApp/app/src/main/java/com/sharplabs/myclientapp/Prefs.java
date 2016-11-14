1579b6b1cc659c22a9ec7ac3b39749ac8879c451
Kirstie Delos Reyes <kdelosre@uci.edu>
2016-11-14 13:12:53 -0800

    Removed duplicate client folder

--- Documents/localrep/191A_SHARP/MyClientApp/MyClientApp/app/src/main/java/com/sharplabs/myclientapp/Prefs.java
+++
@@ -1,9 +0,0 @@
-package com.sharplabs.myclientapp;
-
-/**
- * Created by syamamura on 10/10/2016.
- */
-public class Prefs {
-    public static final String SERVER_URL = "server_url";
-    public static final String SERVER_URL_DEFAULT = "http://ocbstsyw702:8081/";
-}
